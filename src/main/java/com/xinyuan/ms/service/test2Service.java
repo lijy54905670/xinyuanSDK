c7bf18743fda4a50d2e163849c5feeb164478046
ylj <2623890680@qq.com>
2021-01-20 09:10:14 +0800

    1

--- src/main/java/com/xinyuan/ms/service/test2Service.java
+++ src/main/java/com/xinyuan/ms/service/test2Service.java
@@ -29,12 +29,12 @@ public class test2Service {
     public static int code = 5;
 
 
-    public  void initMemberFlowUpload(String m_sDeviceIP) throws InterruptedException {
+    public void initMemberFlowUpload(String m_sDeviceIP) throws InterruptedException {
         // 初始化
         Boolean flag = hCNetSDK.NET_DVR_Init();
-        if (flag){
+        if (flag) {
             System.out.println("初始化成功");
-        }else{
+        } else {
             System.out.println("初始化失败");
         }
         //设置连接时间与重连时间
@@ -55,17 +55,17 @@ public class test2Service {
         m_strLoginInfo.write();
 
         //设备信息, 输出参数
-        int lUserID = hCNetSDK.NET_DVR_Login_V40(m_strLoginInfo,m_strDeviceInfo);
-        if(lUserID< 0){
-            System.out.println("hCNetSDK.NET_DVR_Login_V30()"+"\n" +hCNetSDK.NET_DVR_GetErrorMsg(null));
+        int lUserID = hCNetSDK.NET_DVR_Login_V40(m_strLoginInfo, m_strDeviceInfo);
+        if (lUserID < 0) {
+            System.out.println("hCNetSDK.NET_DVR_Login_V30()" + "\n" + hCNetSDK.NET_DVR_GetErrorMsg(null));
             hCNetSDK.NET_DVR_Cleanup();
             return;
         }
         //设置报警回调函数
-        if (!hCNetSDK.NET_DVR_SetDVRMessageCallBack_V31(this::MsesGCallBack,null )){
-            System.out.println("设置回调函数失败"+hCNetSDK.NET_DVR_GetLastError());
+        if (!hCNetSDK.NET_DVR_SetDVRMessageCallBack_V31(this::MsesGCallBack, null)) {
+            System.out.println("设置回调函数失败" + hCNetSDK.NET_DVR_GetLastError());
             return;
-        }else {
+        } else {
             System.out.println("设置回调函数成功");
         }
         //启用布防
@@ -73,10 +73,9 @@ public class test2Service {
         lpSetupParam.dwSize = 0;
         lpSetupParam.byLevel = 1;//布防优先级：0- 一等级（高），1- 二等级（中）
         lpSetupParam.byAlarmInfoType = 1;//上传报警信息类型: 0- 老报警信息(NET_DVR_PLATE_RESULT), 1- 新报警信息(NET_ITS_PLATE_RESULT)
-        int lAlarmHandle = hCNetSDK.NET_DVR_SetupAlarmChan_V41(lUserID,lpSetupParam);
-        if (lAlarmHandle< 0)
-        {
-            System.out.println("NET_DVR_SetupAlarmChan_V41 error, %d\n"+hCNetSDK.NET_DVR_GetLastError());
+        int lAlarmHandle = hCNetSDK.NET_DVR_SetupAlarmChan_V41(lUserID, lpSetupParam);
+        if (lAlarmHandle < 0) {
+            System.out.println("NET_DVR_SetupAlarmChan_V41 error, %d\n" + hCNetSDK.NET_DVR_GetLastError());
             hCNetSDK.NET_DVR_Logout(lUserID);
             hCNetSDK.NET_DVR_Cleanup();
             return;
@@ -88,13 +87,13 @@ public class test2Service {
         String m_sListenIP = "127.0.0.1";
 
         lListenHandle = hCNetSDK.NET_DVR_StartListen_V30(m_sListenIP, (short) iListenPort, this::MsesGCallBack, null);
-        if(lListenHandle < 0) {
+        if (lListenHandle < 0) {
 //            JOptionPane.showMessageDialog(null, "启动监听失败，错误号:" +  hCNetSDK.NET_DVR_GetLastError());
-        }
-        else {
+        } else {
             System.out.println("启动监听成功");
         }
     }
+
     public boolean MsesGCallBack(int lCommand, HCNetSDK.NET_DVR_ALARMER pAlarmer, Pointer pAlarmInfo, int dwBufLen, Pointer pUser) {
         System.out.println("监听开始11111111111111111");
         try {
@@ -102,42 +101,61 @@ public class test2Service {
             String[] newRow = new String[3];
             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
             SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
-            String imgName =sf.format(new Date())+".jpg";
-            String clImgName ="cl"+sf.format(new Date())+".jpg";
+            String imgName = sf.format(new Date()) + ".jpg";
+            String clImgName = "cl" + sf.format(new Date()) + ".jpg";
             String[] sIP = new String[2];
             System.out.println(lCommand);
-            if (lCommand != 4432){
-                Thread.sleep(5000);
+            if (lCommand != 4432) {
                 System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
             }
-            if (lCommand == 0x1102){
+            if (lCommand == 0x1102) {
                 System.out.println("hahahahahahahah");
             }
             switch (lCommand) {
 
-                case 0x1102:	//交通抓拍的终端图片上传
-                    HCNetSDK.NET_ITS_PLATE_RESULT strItsPlateResult = new HCNetSDK.NET_ITS_PLATE_RESULT();
-                    HCNetSDK.NET_VCA_RULE_ALARM ss = new HCNetSDK.NET_VCA_RULE_ALARM();
+                case 0x1102:    //交通抓拍的终端图片上传
+                    HCNetSDK.NET_VCA_RULE_ALARM strVcaAlarm = new HCNetSDK.NET_VCA_RULE_ALARM();
+                    strVcaAlarm.write();
+                    Pointer pVcaInfo = strVcaAlarm.getPointer();
+                    pVcaInfo.write(0, pAlarmInfo.getByteArray(0, strVcaAlarm.size()), 0, strVcaAlarm.size());
+                    strVcaAlarm.read();
 
-                    ss.write();
-                    Pointer pointer = ss.getPointer();
-                    pointer.write(0,pAlarmInfo.getByteArray(0,ss.size()),0,ss.size());
-                    ss.read();
-
-
-                    int dwAbsTime = ss.dwAbsTime;
-                    int dwPicDataLen = ss.dwPicDataLen;
-                    SimpleDateFormat format =  new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
-                    String d = format.format(dwAbsTime);
-                    short wEventTypeEx = ss.struRuleInfo.wEventTypeEx;
-                    Date date=format.parse(d);
-                    int i = 0;
+                    switch (strVcaAlarm.struRuleInfo.wEventTypeEx) {
+                        case 1:
+                            sAlarmType = sAlarmType + new String("：穿越警戒面") + "，" +
+                                    "_wPort:" + strVcaAlarm.struDevInfo.wPort +
+                                    "_byChannel:" + strVcaAlarm.struDevInfo.byChannel +
+                                    "_byIvmsChannel:" + strVcaAlarm.struDevInfo.byIvmsChannel +
+                                    "_Dev IP：" + new String(strVcaAlarm.struDevInfo.struDevIP.sIpV4);
+                            break;
+                        case 2:
+                            sAlarmType = sAlarmType + new String("：目标进入区域") + "，" +
+                                    "_wPort:" + strVcaAlarm.struDevInfo.wPort +
+                                    "_byChannel:" + strVcaAlarm.struDevInfo.byChannel +
+                                    "_byIvmsChannel:" + strVcaAlarm.struDevInfo.byIvmsChannel +
+                                    "_Dev IP：" + new String(strVcaAlarm.struDevInfo.struDevIP.sIpV4);
+                            break;
+                        case 3:
+                            sAlarmType = sAlarmType + new String("：目标离开区域") + "，" +
+                                    "_wPort:" + strVcaAlarm.struDevInfo.wPort +
+                                    "_byChannel:" + strVcaAlarm.struDevInfo.byChannel +
+                                    "_byIvmsChannel:" + strVcaAlarm.struDevInfo.byIvmsChannel +
+                                    "_Dev IP：" + new String(strVcaAlarm.struDevInfo.struDevIP.sIpV4);
+                            break;
+                        default:
+                            sAlarmType = sAlarmType + new String("：其他行为分析报警，事件类型：")
+                                    + strVcaAlarm.struRuleInfo.wEventTypeEx +
+                                    "_wPort:" + strVcaAlarm.struDevInfo.wPort +
+                                    "_byChannel:" + strVcaAlarm.struDevInfo.byChannel +
+                                    "_byIvmsChannel:" + strVcaAlarm.struDevInfo.byIvmsChannel +
+                                    "_Dev IP：" + new String(strVcaAlarm.struDevInfo.struDevIP.sIpV4);
+                            break;
+                    }
                     break;
 
             }
-            //logger.info("识别信息：---》" +sAlarmType +" ip:"+new String(pAlarmer.sDeviceIP).trim()+"时间:"+dateFormat.format(new Date()));
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
